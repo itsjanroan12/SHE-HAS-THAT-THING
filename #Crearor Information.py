9a74abd39ce2802b030f3e5c0c01b956e28b5641
ABAD, JAN ROAN D <163645999+itsjanroan12@users.noreply.github.com>
2024-03-23 22:45:36 +0800

    Add files via upload

---
+++ #Crearor Information.py
@@ -0,0 +1,20 @@
+#Crearor Information
+#Name: Abad, Jan Roan D.
+#Section: BSCpE 1-2
+#Date: March 23, 2024
+#Faculty: Prof. Danilo Madrigalejos
+
+#The Project paln and pesaudo code
+#Text Effects Handling:
+#We'll create a set of functions to handle special effects for text printing, like typing text slowly or showing text in rainbow colors.
+#Header Design:
+#We'll craft a special header for our program using a creative ASCII art design.
+#Main Program Flow:
+#Our main function will guide the program's actions.
+#It will display the program's header.
+#Using the text effects, it will gradually introduce the user to the Dream Job Generator.
+#Users will be prompted to input their name and dream job.
+#We'll display personalized messages using a fancy font, incorporating the user's inputs.
+#If someone directly runs our script, the main function will execute.
+#Implementation: We'll write the code according to this plan, ensuring everything is correctly structured and syntax is accurate.
+#Testing will be crucial to ensure all features work as expected, including text effects, user input, and the display of ASCII art.
